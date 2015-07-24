fe5b6752a5a99cb063d406d6d988da99920e1dda
probonopd <probono@puredarwin.org>
2015-07-24 10:20:08 +0200

    Update build-decodeir.sh

--- tools/build-decodeir.sh
+++ tools/build-decodeir.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
-
-svn export -q svn://svn.code.sf.net/p/controlremote/code/trunk/decodeir
+# svn export -q svn://svn.code.sf.net/p/controlremote/code/trunk/decodeir
+wget -c http://www.harctoolbox.org/downloads/decodeir.tar.gz
+tar xfvz decodeir.tar.gz
 cd decodeir
 mvn install
-
 # end here if compilation of the shared library is not wanted
-
 autoreconf -fi
 ./configure
 make
-sudo make install 
+sudo make install
